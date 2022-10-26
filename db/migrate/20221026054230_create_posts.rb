748ad35b8b2dcd36ef8c5e3605d4dbe8021b4178
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-10-26 15:31:03 +0800

    adjust root path
    add user id in post table

--- db/migrate/20221026054230_create_posts.rb
+++ db/migrate/20221026054230_create_posts.rb
@@ -3,7 +3,6 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.string :content
-      t.references :user
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_054230) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_070014) do
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.string "content"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
