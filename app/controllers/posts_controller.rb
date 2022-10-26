deeab7721f802d850de71b6ddfefdefe4f8440b5
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-10-26 15:47:01 +0800

    add before action authenticate_user, post params and validate post owner
    done show action
    done edit action
    done show action
    done destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post_params, only: [:show, :edit, :update, :destroy]
+  before_action :validate_post_owner, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.includes(:user).all
   end
@@ -6,4 +10,54 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      flash[:notice] = "Post was created successfully."
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    if @posts.update(post_params)
+      # @post.user = current_user
+      flash[:notice] = "Post was updated successfully."
+      redirect_to posts_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @posts.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post_params
+    @posts = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def validate_post_owner
+    unless @posts.user == current_user
+      flash[:notice] = 'the post not belongs to you'
+      redirect_to posts_path
+    end
+  end
+  end
